609a8f39082b9dc236d3886e32ad8dba93459dc2
Pracdoer <pracdoer@gmail.com>
2020-02-16 19:54:58 +0500

    finialize the first task

--- assignments/Assignemt-1/assignment-1.py
+++ assignments/Assignemt-1/assignment-1.py
@@ -16,8 +16,6 @@ corpus = open('Movies_TV.txt').read()
 corpus = re.sub(r'Domain.*\n', '', corpus)
 rows = corpus.split('\n')
 rows.remove(rows[-1])
-#print(rows)
-#rows2 = rows
 inputData, y = [], []
 for row in rows:
     _, label, _, review = row.split('\t')
@@ -31,47 +29,38 @@ for row in rows:
 print("----------------------")
 
 def normlizing_case(inp):
-	inp2 = []
-	for row in inp:
-		row = row.lower()
-		inp2.append(row)
-	return inp2
+    inp2 = []
+    for row in inp:
+        row = row.lower()
+        inp2.append(row)
+    return inp2
 
 def unwantd_spaces(inp):
-	inp2 = []
-	for row in inp:
-		row = re.sub(' +', ' ', row)
-		inp2.append(row)
-	return inp2
+    inp2 = []
+    for row in inp:
+        row = re.sub(' +', ' ', row)
+        inp2.append(row)
+    return inp2
 
-unwantd_spaces(inputData)
-#print(inputData[1])
-after_data2 = []
-def r_numb_punc(inp):
-	inp2 = []
-	for row in inp:
-		row = re.sub('[!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]', ' ', row).lower()
-		tokens = word_tokenize(row)
-		row = [i for i in tokens if not re.search(r'\d', i)]
-		row = [ps.stem(i) for i in tokens] #stemming
-		row = [wl.lemmatize(i) for i in tokens] #lemmatization
-		inp2.append(row)
-	return inp2
-
-
-def r_stop_words(inp):
-	inp2 = []
-	for row in inp:
-		row = [i for i in row if not i in stop_word]
-		inp2.append(row)
-	return inp2
 
 def r_punc(inp):
-	inp2 = []
-	for row in inp:
-		row = [i for i in row if not i in punc]
-		inp2.append(row)
-	return inp2
+    inp2 = []
+    for row in inp:
+        row = re.sub('[!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~]', ' ', row).lower()
+        inp2.append(row)
+    return unwantd_spaces(inp2)
+
+
+#### Stop Words #####
+def r_stop_words(inp):
+    inp2 = []
+    for row in inp:
+        tokens = word_tokenize(row)
+        row = [i for i in tokens if not i in stop_word]
+        inp2.append(row)
+    return inp2
+
+
 
 def stemming(inp):
 	inp2 = []
@@ -81,19 +70,25 @@ def stemming(inp):
 	return inp2
 
 def lemmitization(inp):
-	inp2 = []
-	for row in inp: 
-		row = [wl.lemmatize(i,'v') for i in row]
-		inp2.append(row)
-	return inp2
+    inp2 = []
+    for row in inp:
+        row = [wl.lemmatize(i) for i in row]
+        inp2.append(row)
+    return inp2
+
+def r_num(inp):
+    inp2 = []
+    for row in inp:
+        row = [i for i in row if not re.search(r'\d', i)]
+        inp2.append(row)
+    return inp2
 
 inputData = unwantd_spaces(inputData)
 inputData = normlizing_case(inputData)
-inputData = r_numb_punc(inputData)
-inputData = r_stop_words(inputData)
-#inputData = r_punc(inputData)
-#inputData = stemming(inputData)
 inputData = r_punc(inputData)
-#inputData = lemmitization(inputData)
-print(inputData[2])
+inputData = r_stop_words(inputData)
+inputData = stemming(inputData)
+inputData = lemmitization(inputData)
+inputData = r_num(inputData)
+print(inputData[1])
 
